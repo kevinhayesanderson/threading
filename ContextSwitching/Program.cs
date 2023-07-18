7c9423b0dcc651451ce22d058fbfcae506190e9d
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-07-18 09:14:01 +0530

    Context Switching

--- ContextSwitching/Program.cs
+++ ContextSwitching/Program.cs
@@ -1,13 +1,15 @@
-﻿namespace threading
+﻿namespace ContextSwitching
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Thread thread = new Thread(WriteUsingNewThread);
+            thread.Name = "Worker 1";
             //Worker Thread
             thread.Start();
             //Main Thread
+            Thread.CurrentThread.Name = "Main";
             for (int i = 0; i < 1000; i++)
             {
                 Console.Write(" A" + i + " ");
