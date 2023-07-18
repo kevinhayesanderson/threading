193ca4ca44ab66f9503a3b30bb767aa16757c6e9
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-07-18 08:06:53 +0530

    Add project files.

---
+++ threading/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(WriteUsingNewThread);
+            //Worker Thread
+            thread.Start();
+            //Main Thread
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(" A" + i + " ");
+            }
+        }
+
+        private static void WriteUsingNewThread(object? obj)
+        {
+            for (int i = 0; i< 1000; i++)
+            {
+                Console.Write(" Z" + i+ " ");
+            }
+        }
+    }
+}
