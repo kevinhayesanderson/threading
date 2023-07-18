11604301c0414954b4993b811f58e44a09fab562
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-07-18 09:29:39 +0530

    Shared Resources

---
+++ SharedResources/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SharedResources
+{
+    internal class Program
+    {
+        private static bool isCompleted;
+        static readonly object lockCompleted = new object();
+
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(HelloWorld);
+            //Worker Thread
+            thread.Start();
+            //Main Thread
+            HelloWorld();
+        }
+
+        private static void HelloWorld()
+        {
+            lock (lockCompleted)
+            {
+                if (!isCompleted)
+                {
+                    isCompleted = true;
+                    Console.WriteLine("Hello world should print only once");
+                }
+            }
+        }
+    }
+}
