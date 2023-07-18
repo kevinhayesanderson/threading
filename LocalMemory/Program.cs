173bcd11b16ea9504aa2e46fd1fd9e6ebd45d4ba
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-07-18 09:48:35 +0530

    Local Memory

---
+++ LocalMemory/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LocalMemory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Worker Thread
+            new Thread(PrintOneToThirty).Start();
+
+            //Main Thread
+            PrintOneToThirty();
+        }
+
+        private static void PrintOneToThirty()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Console.Write(i + 1 + " ");
+            }
+        }
+    }
+}
