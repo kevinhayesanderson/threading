6d0eee8595f8411e64d1f71ead217df6e3fdc3ee
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-07-19 20:08:17 +0530

    Exceptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo();
+        }
+
+        private static void Demo()
+        {
+            //try
+            //{
+            //Worker Thread
+            new Thread(Execute).Start();
+            //}
+            ////Main Thread Catch
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+        }
+
+        static void Execute() ///catch where you expect exception 
+        {
+            try
+            {
+                throw null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
