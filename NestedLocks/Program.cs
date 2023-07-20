473c7bfaa820f6b5c735dc7dd61eef910b704231
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-07-20 12:43:33 +0530

    Nested Locks

---
+++ NestedLocks/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NestedLocks
+{
+    internal class Program
+    {
+        static object lock1  = new object();
+        static void Main(string[] args)
+        {
+           lock(lock1) //Parent lock holds the lock on the resource
+            {
+                DoSth();
+            }
+        }
+
+        private static void DoSth()
+        {
+            lock(lock1)
+            {
+                Task.Delay(2000).Wait();
+                AnotherMethod();
+            }
+        }
+
+        private static void AnotherMethod()
+        {
+            lock (lock1)
+            {
+                Console.WriteLine("Another Method");
+            }
+        }
+    }
+}
