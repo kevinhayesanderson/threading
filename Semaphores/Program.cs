2d059338f7cbb76a285469d5cec9177b235eea5e
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-07-20 13:42:15 +0530

    Semaphores

---
+++ Semaphores/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+
+namespace Semaphores
+{
+    internal class Program
+    {
+        static SemaphoreSlim semaphoreSlim = new SemaphoreSlim(3);
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <10; i++)
+            {
+                new Thread(EnterSemaphore).Start(i+1);
+            }
+        }
+
+        private static void EnterSemaphore(object? id)
+        {
+            Console.WriteLine($"{id} is waiting to be part of the club");
+            semaphoreSlim.Wait();
+            Console.WriteLine($"{id} is part of the club");
+            Thread.Sleep(1000 / (int)id);
+            Console.WriteLine($"{id} left the club");
+        }
+    }
+}
