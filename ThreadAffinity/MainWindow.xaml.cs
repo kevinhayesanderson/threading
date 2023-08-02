e5f81d39f5c5e34975254803ac07647d34230338
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-08-02 21:08:01 +0530

    Thread safety and thread affinity

---
+++ ThreadAffinity/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThreadAffinity
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Task.Factory.StartNew(ChangeText);
+        }
+
+        private void ChangeText()
+        {
+            Thread.Sleep(2500);
+            UpdateText("changed text");
+        }
+
+        private void UpdateText(string text)
+        {
+            //// txtMessage.Text = text;
+            /// System.InvalidOperationException
+            /// Message = The calling thread cannot access this object because a different thread owns it.
+
+            Dispatcher.Invoke(() => { txtMessage.Text = text; });
+        }
+    }
+}
---
+++ ThreadSafety/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ThreadSafety
+{
+    internal class Program
+    {
+        static Dictionary<int, string> items = new Dictionary<int, string>();
+        static void Main(string[] args)
+        {
+            var task1 = Task.Factory.StartNew(AddItem);
+            var task2 = Task.Factory.StartNew(AddItem);
+            var task3 = Task.Factory.StartNew(AddItem);
+            var task4 = Task.Factory.StartNew(AddItem);
+            var task5 = Task.Factory.StartNew(AddItem);
+            Task.WaitAll(task1, task2, task3, task4, task5);
+        }
+
+        private static void AddItem()
+        {
+            lock (items)
+            {
+                Console.WriteLine($"Lock 1 acquired by {Task.CurrentId}");
+                items.Add(items.Count, $"data {items.Count}");
+            }
+
+            Dictionary<int, string> dictionary;
+            lock (items)
+            {
+                Console.WriteLine($"Lock 2 acquired by {Task.CurrentId}");
+                dictionary = items;
+                foreach (var (key, value) in dictionary)
+                {
+                    Console.WriteLine($"{key} {value}");
+                }
+            }
+
+            
+        }
+    }
+}
