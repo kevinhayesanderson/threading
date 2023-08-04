da7e5b4b7c73402d6873b52cd700c4ebc30bbe18
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-08-04 19:53:06 +0530

    CountdownEvent

---
+++ Countdown/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Countdown
+{
+    internal class Program
+    {
+        private static CountdownEvent countdownEvent = new CountdownEvent(5);
+
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(DoSomething);
+            Task.Factory.StartNew(DoSomething);
+            Task.Factory.StartNew(DoSomething);
+            Task.Factory.StartNew(DoSomething);
+            Task.Factory.StartNew(DoSomething);
+            countdownEvent.Wait();
+            Console.WriteLine("Signal has been called 5 times");
+        }
+
+        private static void DoSomething()
+        {
+            Thread.Sleep(2500);
+            Console.WriteLine($"{Task.CurrentId} is calling signal");
+            countdownEvent.Signal();
+        }
+    }
+}
