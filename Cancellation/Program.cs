4af81652833052bbd51159afaf61855cceb23d46
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-08-10 10:20:31 +0530

    Parallel and Parallel Cancellation

---
+++ Cancellation/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Cancellation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Enumerable.Range(0, 1000000).ToArray();
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+            ParallelOptions parallelOptions = new ParallelOptions();
+            parallelOptions.CancellationToken = cancellationTokenSource.Token;
+            parallelOptions.MaxDegreeOfParallelism = System.Environment.ProcessorCount;
+
+            Console.WriteLine("Press 'x' to cancel");
+
+            Task.Factory.StartNew(() =>
+            {
+                if (Console.ReadKey().KeyChar == 'x')
+                {
+                    cancellationTokenSource.Cancel();
+                }
+            });
+
+            long total = 0;
+            try
+            {
+                Parallel.For(0,
+                    list.Length,
+                    parallelOptions,
+                    () => 0,
+                    (count, parallelLoopState, subTotal) =>
+                    {
+                        parallelOptions.CancellationToken.ThrowIfCancellationRequested();
+                        subTotal += list[count];
+                        return subTotal;
+                    },
+                    (x) =>
+                    {
+                        Interlocked.Add(ref total, x);
+                    });
+            }
+            catch (OperationCanceledException opEx)
+            {
+                Console.WriteLine("Cancelled: " + opEx.Message);
+            }
+            finally { cancellationTokenSource.Cancel(); }
+
+            Console.WriteLine("Final sum: " + total);
+        }
+    }
+}
---
+++ ParallelvsNormal/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+
+namespace ParallelvsNormal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Directory.GetCurrentDirectory();
+            var files = Directory.GetFiles(path + @"\pictures", "*.jpg");
+            var alteredPath = path + @"\alteredPath";
+            Directory.CreateDirectory(alteredPath);
+
+            ParallelExecution(files, alteredPath);
+
+            NormalExecution(files, alteredPath);
+        }
+
+        private static void NormalExecution(string[] files, string alteredPath)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            foreach (var currentFile in files)
+            {
+                var file = Path.GetFileName(currentFile);
+                using var fileBitMap = new Bitmap(currentFile);
+                fileBitMap.RotateFlip(RotateFlipType.Rotate270FlipXY);
+                fileBitMap.Save(Path.Combine(alteredPath, file));
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}");
+            }
+            Console.WriteLine("NormalExecution:" + stopwatch.ElapsedMilliseconds);
+            stopwatch.Stop();
+        }
+
+        private static void ParallelExecution(string[] files, string alteredPath)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            Parallel.ForEach(files, currentFile =>
+            {
+                var file = Path.GetFileName(currentFile);
+                using var fileBitMap = new Bitmap(currentFile);
+                fileBitMap.RotateFlip(RotateFlipType.Rotate270FlipXY);
+                fileBitMap.Save(Path.Combine(alteredPath, file));
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}");
+            });
+            Console.WriteLine("ParallelExecution:" + stopwatch.ElapsedMilliseconds);
+            stopwatch.Stop();
+        }
+    }
+}
