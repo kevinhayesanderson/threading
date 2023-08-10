e204f89f9043ca240ee2e39fb4fb025b86c95a06
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-08-10 10:45:44 +0530

    ContinuationWithState and TaskCompletionSource

---
+++ ContinuationWithState/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ContinuationWithState
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<DateTime> task = Task.Run(DoSomething);
+            List<Task<DateTime>> continuationTasks = new List<Task<DateTime>>();
+            for (int i = 0; i < 3; i++)
+            {
+                task = task.ContinueWith((x, y) => DoSomething(), new Person { Id=i});
+                continuationTasks.Add(task);
+            }
+            task.Wait(); 
+            foreach (var continuation in continuationTasks)
+            {
+                Person person = continuation.AsyncState as Person;
+                Console.WriteLine($"Task finished at {continuation.Result}. Person id is {person.Id}");
+            }
+        }
+
+        public static DateTime DoSomething()
+        {
+            return DateTime.Now;
+        }
+    }
+
+    internal class Person
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ TaskCompletionSourceDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace TaskCompletionSourceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskCompletionSource<Product> taskCompletionSource = new TaskCompletionSource<Product>();
+            Task<Product> lazyTask = taskCompletionSource.Task;
+
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2000);
+                taskCompletionSource.SetResult(new Product()
+                {
+                    Id = 1,
+                    Name = "Test Product"
+                });
+            });
+
+            Task.Factory.StartNew(() =>
+            {
+                if (Console.ReadKey().KeyChar == 'x')
+                {
+                    Product result = lazyTask.Result;
+                    Console.WriteLine($"Result is {result.Name}");
+                }
+            });
+
+            Thread.Sleep(5000);
+            Console.ReadLine();
+        }
+    }
+
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
